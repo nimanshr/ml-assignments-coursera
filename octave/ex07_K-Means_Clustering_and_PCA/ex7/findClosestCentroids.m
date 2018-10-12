d8f624e7ec9e2657c21ff9da661132572549e254
Nima Nooshiri <nima.nooshiri@gmail.com>
2018-10-12 02:17:43 +0200

    Added assignment 7 (week 8)

---
+++ octave/ex07_K-Means_Clustering_and_PCA/ex7/findClosestCentroids.m
@@ -0,0 +1,43 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+for i_sample=1:size(X, 1)
+    xi = X(i_sample, :);
+    
+    dist_min = inf;
+    for i_centroid=1:K
+        ci = centroids(i_centroid, :);
+        dist = norm(xi-ci, 2);
+
+        if dist < dist_min
+            dist_min = dist;
+            idx(i_sample) = i_centroid;
+        end
+    end
+    
+end
+
+% =============================================================
+
+end
+
